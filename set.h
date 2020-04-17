66770c53fcde65677ac90cab54a1d6d297b4b7d2
Jim Eli <31859133+JimEli@users.noreply.github.com>
2020-04-17 11:08:31 -0700

    Update set.h

--- set.h
+++ set.h
@@ -1,6 +1,3 @@
-#ifndef _SET_H_
-#define _SET_H_
-
 #include "tree_with_parent.h"
 
 template <class T>
@@ -30,7 +27,8 @@ struct set : public tree<T>
 			return;
 		}
 
-		auto s1 = s.begin(), s2 = this->begin();
+		auto s1 = s.begin();
+		auto s2 = this->begin();
 
 		while (s1 != s.end() && s2 != this->end())
 		{
@@ -53,7 +51,8 @@ struct set : public tree<T>
 		if (this != &s)
 		{
 
-			auto s1 = this->begin(), s2 = s.begin();
+			auto s1 = this->begin();
+			auto s2 = s.begin();
 
 			while (s1 != this->end() && s2 != s.end())
 			{
@@ -79,8 +78,8 @@ struct set : public tree<T>
 		}
 	}
 
-  // Reject duplicates.
 	void insert(const T data) { if (!tree<T>::search(data)) tree<T>::add(data); }
-};
 
-#endif
+	T lowerBound() const { return *tree<T>::begin(); }
+	T upperBound() const { return *tree<T>::rbegin(); }
+};
